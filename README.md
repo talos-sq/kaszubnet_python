be936be3622590d9c1ed68a96eab3040da3ed795
blackieghoster <blackieghoster@gmail.com>
2022-02-07 16:19:07 +0100

    Django setup

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # kaszubnet-python
-New iteration of kaszubnet application.
+New iteration of kaszubnet application builded on Django framework.
---
+++ kaszubnet/kaszubnet_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class KaszubnetAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'kaszubnet_app'
