0e3d771047a4e9926da95bb8c61e421f6f4866f5
blackieghoster <blackieghoster@gmail.com>
2024-02-10 04:03:58 +0100

    static files fix 6

--- kaszubnet/urls.py
+++ kaszubnet/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-import settings
 from kaszubnet_app.views import *
 
 urlpatterns = [
