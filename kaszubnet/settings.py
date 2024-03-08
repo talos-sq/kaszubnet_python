c34b351e8a30a8aaa0241229a0af953474e7986e
blackieghoster <blackieghoster@gmail.com>
2024-03-09 00:30:28 +0100

    Fixes

--- kaszubnet/settings.py
+++ kaszubnet/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-v3!0u#he6_i)_&y93^@w*inuih^i+#$tkc#y19qm%kf3p29d+-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
@@ -123,13 +123,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-# STATIC_URL = 'static/'
-STATIC_ROOT = "/home/site/wwwroot/kaszubnet_app/static"
-
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    "/home/site/wwwroot/kaszubnet_app/static/",
-]
+STATIC_URL = 'static/'
+# STATIC_ROOT = "/home/site/wwwroot/kaszubnet_app/static"
+#
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+#     "/home/site/wwwroot/kaszubnet_app/static/",
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
